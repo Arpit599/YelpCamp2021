3f75819fde403ab254abc76b5a61a840b6559ea9
Arpit Bharadwaj <arpit.bharadwaj5@gmail.com>
2021-03-18 12:17:14 +0530

    Added cluster map and tweaked its style

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -1,11 +1,20 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',  // Id of the div tag used to render the map, in our case the id is "map"
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: campground.geometry.coordinates,
     // center: [10,56],
     zoom: 10
 });
 
-const marker = new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        //Offset is the distance of the popup from the pin or marker
+        new mapboxgl.Popup({ offset: 25, closeOnClick: true })
+            .setHTML(
+            `<h3>${campground.title}</h3><p>${campground.location}</p>`
+        )
+    )
+    .addTo(map);
 // const marker = new mapboxgl.Marker().setLngLat([10, 56]).addTo(map);
--- seeds/index.js
+++ seeds/index.js
@@ -20,28 +20,40 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDb = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 20; i++){
+    for (let i = 0; i < 300; i++){
         const price = Math.floor(Math.random() * 100);
+        const randomData = sample(cities);
+        const sampleCity = randomData.city;
+        const sampleState = randomData.state;
+        // console.log(randomData);
+        // console.log(sampleCity);
+        // console.log(sampleState);
+        // console.log(randomData.longitude);
+        // console.log(randomData.latitude);
         const newCampground = new Campground({
-            location: `${sample(cities).city}, ${sample(cities).state}`,
+            location: `${sampleCity}, ${sampleState}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor omnis magni officia libero aperiam quae aliquam? Explicabo vero a praesentium alias, quod delectus. Nobis ducimus corporis, modi velit veniam minima?',
             price: price,
             author: '60435e9a972bda528825019d',
             images: [
-                        {
-                        url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804689/Yelpcamp/mzizqmr7f8dhzvv6sset.jpg',
-                        filename: 'Yelpcamp/mzizqmr7f8dhzvv6sset'
-                        },
-                        {
-                        url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804692/Yelpcamp/t5jmhwwoj8qpgtkqttnc.jpg',
-                        filename: 'Yelpcamp/t5jmhwwoj8qpgtkqttnc'
-                        },
-                        {
-                        url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804690/Yelpcamp/nxhqkwjxsvjwwmexds2a.jpg',
-                        filename: 'Yelpcamp/nxhqkwjxsvjwwmexds2a'
-                        }
-                    ]
+                {
+                url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804689/Yelpcamp/mzizqmr7f8dhzvv6sset.jpg',
+                filename: 'Yelpcamp/mzizqmr7f8dhzvv6sset'
+                },
+                {
+                url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804692/Yelpcamp/t5jmhwwoj8qpgtkqttnc.jpg',
+                filename: 'Yelpcamp/t5jmhwwoj8qpgtkqttnc'
+                },
+                {
+                url: 'https://res.cloudinary.com/dzre5qhvl/image/upload/v1615804690/Yelpcamp/nxhqkwjxsvjwwmexds2a.jpg',
+                filename: 'Yelpcamp/nxhqkwjxsvjwwmexds2a'
+                }
+            ],
+            geometry: {
+                type: "Point",
+                coordinates: [randomData.longitude, randomData.latitude]
+            }
         })
         await newCampground.save();
     }
